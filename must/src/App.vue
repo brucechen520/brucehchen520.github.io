ea9bcd83f821d46e76a3bac10a106955c5786902
brucechen520 <gibe329apt978@gmail.com>
2018-12-29 11:06:26 +0800

    Dropdown modified

--- must/src/App.vue
+++ must/src/App.vue
@@ -1,82 +1,77 @@
 <template>
   <div id="app">
-      <dropdown trigger="hover">
-        <button class=""><router-link to="/Home">§回會員系統</router-link></button>
-      </dropdown>
-      <dropdown trigger="hover">
-        <button class="">§網站列表</button>
-        <div slot="content">
-            <menus>
-              <menu-item >
-                <router-link to="/website-list/business-card/OpenBusinessCard">
-                  公開名片區
-                </router-link>
-              </menu-item>
-              <menu-item >
-                <router-link to="/website-list/business-card/CloseBusinessCard">
-                  不公開名片區
-                </router-link>
-              </menu-item>
-              <menu-item >
-                <router-link to="/website-list/business-card/OBUpload">
-                  公開名片區上傳
-                </router-link>
-              </menu-item>
-              <menu-item >
-                <router-link to="/website-list/business-card/CBUpload">
-                  不公開名片區上傳
-                </router-link>
-              </menu-item>
-              <menu-item >
-                <router-link to="/website-list/website/WebsiteQuery">
-                  網站查詢
-                </router-link>
-              </menu-item>
-              <menu-item >
-                <router-link to="/website-list/website/WebsiteFilling">
-                  網站填寫
-                </router-link>
-              </menu-item>
-            </menus>
+      <div class="dropdown">
+        <button class="dropbtn"><router-link to="/Home">§回會員系統</router-link></button>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">§網站列表</button>
+        <div class="dropdown-content">
+        <a>
+          <router-link to="/website-list/business-card/OpenBusinessCard">
+            公開名片區
+          </router-link>
+        </a>
+        <a>
+          <router-link to="/website-list/business-card/CloseBusinessCard">
+            不公開名片區
+          </router-link>
+        </a>
+        <a>
+          <router-link to="/website-list/business-card/OBUpload">
+            公開名片區上傳
+          </router-link>
+        </a>
+        <a>
+          <router-link to="/website-list/business-card/CBUpload">
+            不公開名片區上傳
+          </router-link>
+        </a>
+        <a>
+          <router-link to="/website-list/website/WebsiteQuery">
+            網站查詢
+          </router-link>
+        </a>
+        <a>
+          <router-link to="/website-list/website/WebsiteFilling">
+            網站填寫
+          </router-link>
+        </a>
         </div>
-      </dropdown>
-      <dropdown trigger="hover">
-        <button class="">§工作媒合</button>
-        <div slot="content">
-            <menus>
-              <menu-item >
-                <router-link to="/job-matching/JobVacancies">
-                  職缺填寫
-                </router-link>
-              </menu-item>
-              <menu-item >
-                <router-link to="/job-matching/ProjectFilling">
-                  專案填寫
-                </router-link>
-              </menu-item>
-              <menu-item >
-                <router-link to="/personal-skill/SkillFilling">
-                  專長填寫
-                </router-link>
-              </menu-item>
-              <menu-item >
-                <router-link to="/job-query/JobQuery">
-                  工作查詢
-                </router-link>
-              </menu-item>
-              <menu-item >
-                <router-link to="/job-query/FindMan">
-                  人才查詢
-                </router-link>
-              </menu-item>
-            </menus>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">§工作媒合</button>
+        <div class="dropdown-content">
+          <a>
+            <router-link to="/job-matching/JobVacancies">
+              職缺填寫
+            </router-link>
+          </a>
+          <a>
+            <router-link to="/job-matching/ProjectFilling">
+              專案填寫
+            </router-link>
+          </a>
+          <a>
+            <router-link to="/personal-skill/SkillFilling">
+              專長填寫
+            </router-link>
+          </a>
+          <a>
+            <router-link to="/job-query/JobQuery">
+              工作查詢
+            </router-link>
+          </a>
+          <a>
+            <router-link to="/job-query/FindMan">
+              人才查詢
+            </router-link>
+          </a>
         </div>
-      </dropdown>
-      <dropdown trigger="hover">
-        <button class=""><router-link to="/Logout">§登出</router-link></button>
-        <div slot="content">
-        </div>
-      </dropdown>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn"><router-link to="/Logout">§登出</router-link></button>
+      </div>
+      
       <router-view></router-view>
   </div>
 </template>
@@ -90,3 +85,44 @@ export default {
   }
 }
 </script>
+<style scoped>
+    .dropbtn {
+      background-color: #CDEE82;
+      padding: 16px;
+      font-size: 16px;
+      color:#F17CAF;
+      border: none;
+      cursor: pointer;
+    }
+
+    .dropdown {
+      position: relative;
+      display: inline-block;
+    }
+
+    .dropdown-content {
+      display: none;
+      position: absolute;
+      background-color: #f9f9f9;
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      z-index: 1;
+    }
+
+    .dropdown-content a {
+      color: black;
+      padding: 12px 16px;
+      text-decoration: none;
+      display: block;
+    }
+
+    .dropdown-content a:hover {background-color: #f1f1f1}
+
+    .dropdown:hover .dropdown-content {
+      display: block;
+    }
+
+    .dropdown:hover .dropbtn {
+      background-color: #3e8e41;
+    }
+</style>
