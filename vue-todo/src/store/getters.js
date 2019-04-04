53f5c6238237ed5304acf74c5c80054226f8636b
brucechen520 <gibe329apt978@gmail.com>
2019-04-05 00:20:20 +0800

    刪除cookie等其他資料

--- vue-todo/src/store/getters.js
+++
@@ -1,1 +0,0 @@
-export const getTodo = state => { return state.todos }
--- vue-todo/src/store/mutations_type.js
+++
@@ -1,5 +0,0 @@
-export const EDIT_TODO = 'EDIT_TODO';
-export const ADD_TODO = 'ADD_TODO';
-export const TODO_COMPLETED = 'TODO_COMPLETED';
-export const DELETE_TODO = 'DELETE_TODO';
-export const CHECKED_TODO = 'CHECKED_TODO';
