df932c33b76469a7475ac8ff517064ff1bd1857d
Parsons <gongyingparsons@gmail.com>
2020-01-17 18:49:42 +0800

    新增API api_vacancy_get api_vacancy_comfirm

---
+++ must/api/api_vacancy_comfirm.php
@@ -0,0 +1,32 @@
+<?
+    session_start();
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    header('Access-Control-Allow-Methods: *');
+    include "../db_config.php" ;
+    include_once '../phpclass.php';
+    $json = array();
+    $user = unserialize($_SESSION['mem']);
+    //$ULEVEL = $tmp['ULEVEL'];
+    //$json['ULEVEL'] = $ULEVEL;
+    if(is_object($user)) {
+      $user->GetMemberData();
+      $user->GetMemberConfirm();
+    }
+    
+    $webData = array() ;
+      $update_obj = json_decode(file_get_contents('php://input'));
+		  $str = "UPDATE `alumnidata`.`Industry_Vacancy_List` SET `suggestion` = '".$update_obj->suggestion."',
+				`examined` = '".$update_obj->examined."',
+				`adminer` = '".$user->Mem_Se."',
+				`verifyTime` = '".time()."'
+      WHERE (`JB_Id` = '".$update_obj->id."');";
+		  $list = mysql_query($str);
+		  if($list === FALSE) { // 資料庫有沒有 FALSE
+        $json['error'] = mysql_error();
+		  }
+		  else{
+			  $json['code'] = 'success';
+		  }
+		  echo json_encode($json);
+?>
---
+++ must/api/api_vacancy_get.php
@@ -0,0 +1,101 @@
+<?
+    session_start();
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    header('Access-Control-Allow-Methods: *');
+    include "../db_config.php" ;
+    include_once '../phpclass.php';
+    $Mail = new MailSender();
+    $json = array();
+    $user = unserialize($_SESSION['mem']);
+    //$ULEVEL = $tmp['ULEVEL'];
+    //$json['ULEVEL'] = $ULEVEL;
+    if(is_object($user)) {
+      $user->GetMemberData();
+      $user->GetMemberConfirm();
+    }
+    
+    $webData = array() ;
+	  $str = "SELECT * FROM alumnidata.`Industry_Vacancy_List` where examined != '1'";
+    $list = mysql_query($str);
+		//echo $str;
+    if($list === FALSE) { // 資料庫有沒有 FALSE
+        $json['error'] = mysql_error();
+        echo json_encode($json);
+    } else{
+      while ($row = mysql_fetch_array($list, MYSQL_ASSOC)) {
+        $now_row = new stdClass;
+        $now_row->JB_Id    =  $row[JB_Id];
+        $now_row->company_Name    =  $row[CP_Name];
+        $now_row->company_Website    =  $row[CP_Website];
+        $now_row->Name  =  $row[JB_Name];
+        $now_row->description  =  $row[JB_Description];
+        $now_row->location    =  $row[JB_location];
+        $now_row->duty    =  $row[JB_duty];
+        $now_row->offer    =  $row[JB_Offer];
+        $now_row->officeHour  =  $row[JB_officeHour];
+        $now_row->trip  =  $row[JB_trip];
+        $now_row->vacation  =  $row[JB_vacation];
+        $now_row->demandMans  =  $row[JB_demandMans];
+        $now_row->academicRequire  =  $row[JB_academicRequire];
+        $now_row->exprience  =  $row[JB_trip];
+        $now_row->others  =  $row[JB_others];
+        $now_row->welfare  =  $row[JB_welfare];
+        $now_row->contact_Name  =  $row[CT_Man];
+        $now_row->contact_Mail  =  $row[CT_Mail];
+        $now_row->contact_Phone  =  $row[CT_Phone];
+        $now_row->contact_Time  =  $row[CT_Time];
+        $now_row->examined = $row[examined];
+        $now_row->modify  =  date("Y-m-d", $row[modify]);
+        $now_row->suggestion  =  $row[suggestion];
+        $now_row->adminer  =  $row[adminer];
+        $now_row->verifyTime  =  date("Y-m-d", $row[verifyTime]);
+        $now_row->type = array() ;
+        $now_row->language = array() ;
+        $now_row->skills = array() ;
+        //取得Type資料
+        $str2 = "SELECT * FROM alumnidata.`Industry_Vacancy_Type` WHERE JB_Id = $row[JB_Id]";
+        $list2 = mysql_query($str2);
+
+        while ($row2 = mysql_fetch_array($list2, MYSQL_ASSOC)) {
+          $now_row2 = new stdClass;
+          $now_row2->id = $row2[serial];
+          $now_row2->title = $row2[JB_Title];
+          $now_row2->content = $row2[JB_Type];
+          array_push($now_row->type, $now_row2);
+        }
+        //取得language資料
+        $str2 = "SELECT * FROM alumnidata.`Industry_Vacancy_Language` WHERE JB_Id = $row[JB_Id]";
+        $list2 = mysql_query($str2);
+
+        while ($row2 = mysql_fetch_array($list2, MYSQL_ASSOC)) {
+          $now_row2 = new stdClass;
+          $now_row2->id = $row2[serial];
+          $now_row2->type = $row2[JB_Language];
+          $now_row2->listen = $row2[listen];
+          $now_row2->speak = $row2[speak];
+          $now_row2->read = $row2[read];
+          $now_row2->write = $row2[write];
+          array_push($now_row->language, $now_row2);
+        }
+        //取得skills資料
+        $str2 = "SELECT * FROM alumnidata.`Industry_Vacancy_Skill` WHERE JB_Id = $row[JB_Id]";
+        $list2 = mysql_query($str2);
+
+        while ($row2 = mysql_fetch_array($list2, MYSQL_ASSOC)) {
+          $now_row2 = new stdClass;
+          $now_row2->id = $row2[serial];
+          $now_row2->value = $row2[JB_Skill];
+          array_push($now_row->skills, $now_row2);
+        }
+        
+        array_push($webData, $now_row);
+      }
+      mysql_close($link);
+      $json['data'] = $webData;
+      $result = new stdClass();
+      $result->code = 'success';
+      $result->data = $webData;
+      echo json_encode($result);
+    }
+?>
