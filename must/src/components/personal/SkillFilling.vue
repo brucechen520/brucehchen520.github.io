accefb29d00c1d44bdf69bb83a335e35b8eeaa66
Parsons <gongyingparsons@gmail.com>
2020-07-03 19:03:51 +0800

    1.個人專區/基本資料RWD控制(手機板) 2.個人專區/基本資料/個人資料修改RWD控制(手機板)

--- must/src/components/personal/SkillFilling.vue
+++ must/src/components/personal/SkillFilling.vue
@@ -1,39 +1,39 @@
 <template>
-  <div class="container">
+  <div>
     <div v-if="!isModify && stateResumeData.Mem_Se != null">
     <h1>我的資料</h1>
     <b-button variant="success" @click="toModifyData" class="mb-2">修改資料</b-button>
     <b-card :title="users.name" :sub-title="users.level+'級'" border-variant="Success">
         <b-card-body>
             <b-row class="border-bottom p-2">
-                <b-col>信箱:</b-col><b-col cols="10">{{stateResumeData.mail}}</b-col>
+                <b-col cols="12" md="3">信箱:</b-col><b-col cols="12" md="9">{{stateResumeData.mail}}</b-col>
             </b-row>
             <b-row class="border-bottom p-2">
-                <b-col>手機:</b-col><b-col cols="10">{{stateResumeData.cellphone}}</b-col>
+                <b-col cols="12" md="3">手機:</b-col><b-col cols="12" md="9">{{stateResumeData.cellphone}}</b-col>
             </b-row>
             <b-row class="border-bottom p-2">
-                <b-col>技能專長:</b-col><b-col cols="10">{{stateResumeData.expertise}}</b-col>
+                <b-col cols="12" md="3">技能專長:</b-col><b-col cols="12" md="9">{{stateResumeData.expertise}}</b-col>
             </b-row>
             <b-row class="border-bottom p-2">
-                <b-col>作品:</b-col><b-col cols="10">{{stateResumeData.works}}</b-col>
+                <b-col cols="12" md="3">作品:</b-col><b-col cols="12" md="9">{{stateResumeData.works}}</b-col>
             </b-row>
             <b-row class="border-bottom p-2">
-                <b-col>證照:</b-col><b-col cols="10">{{stateResumeData.license}}</b-col>
+                <b-col cols="12" md="3">證照:</b-col><b-col cols="12" md="9">{{stateResumeData.license}}</b-col>
             </b-row>
             <b-row class="border-bottom p-2">
-                <b-col>自我介紹:</b-col><b-col cols="10"><pre>{{stateResumeData.biography}}</pre></b-col>
+                <b-col cols="12" md="3">自我介紹:</b-col><b-col cols="12" md="9"><pre>{{stateResumeData.biography}}</pre></b-col>
             </b-row>
         </b-card-body>
         <b-card-text class="small text-muted">隱私設定</b-card-text>
         <b-card-body>
             <b-row class="border-bottom p-2">
-                <b-col>姓名:</b-col><b-col cols="10">{{permitdesc[stateResumeData.permit.name]}}</b-col>
+                <b-col>姓名:</b-col><b-col  cols="8" md="9">{{permitdesc[stateResumeData.permit.name]}}</b-col>
             </b-row>
             <b-row class="border-bottom p-2">
-                <b-col>信箱:</b-col><b-col cols="10">{{permitdesc[stateResumeData.permit.mail]}}</b-col>
+                <b-col>信箱:</b-col><b-col  cols="8" md="9">{{permitdesc[stateResumeData.permit.mail]}}</b-col>
             </b-row>
             <b-row class="border-bottom p-2">
-                <b-col>手機:</b-col><b-col cols="10">{{permitdesc[stateResumeData.permit.phone]}}</b-col>
+                <b-col>手機:</b-col><b-col  cols="8" md="9">{{permitdesc[stateResumeData.permit.phone]}}</b-col>
             </b-row>
         </b-card-body>
     </b-card>
@@ -43,15 +43,15 @@
         <ValidationObserver v-slot="{ valid }">
             <b-form>
                 <b-row>
-                    <b-col cols=8><b-form-group label="姓名:" label-for="input-name" label-cols=1>
+                    <b-col cols=12 md="7"><b-form-group label="姓名:" label-for="input-name" label-cols=4 label-cols-md=2>
                             <b-form-input id="input-name"  :value="users.name" type="text" readonly></b-form-input>
                     </b-form-group></b-col>
-                    <b-col cols=4><b-form-group label="是否公開:" label-for="input-2" label-cols=4>
+                    <b-col cols=12 md="5"><b-form-group label="是否公開:" label-for="input-2" label-cols=4 label-cols-md=4>
                             <b-form-select v-model="modifyData.permit.name" :options="permitOption"></b-form-select>
                     </b-form-group></b-col>
                 </b-row>
                 <b-row>
-                    <b-col cols=8><b-form-group label="信箱:" label-for="input-email" label-cols=1>
+                    <b-col cols=12 md="7"><b-form-group label="信箱:" label-for="input-email" label-cols=4 label-cols-md=2>
                             <ValidationProvider rules="required|email" v-slot="{ valid, errors }">
                             <b-form-input id="input-email" :state="valid" v-model="modifyData.mail" type="email" required placeholder="輸入信箱"></b-form-input>
                             <b-form-invalid-feedback :state="valid">
@@ -59,12 +59,12 @@
                             </b-form-invalid-feedback>
                             </ValidationProvider>
                     </b-form-group></b-col>
-                    <b-col cols=4><b-form-group label="是否公開:" label-for="input-2" label-cols=4>
+                    <b-col cols=12 md="5"><b-form-group label="是否公開:" label-for="input-2" label-cols=4>
                         <b-form-select v-model="modifyData.permit.mail" :options="permitOption"></b-form-select>
                     </b-form-group></b-col>
                 </b-row>         
                 <b-row>
-                    <b-col cols=8><b-form-group label="手機:" label-for="input-phone" label-cols=1>
+                    <b-col cols=12 md="7"><b-form-group label="手機:" label-for="input-phone" label-cols=4 label-cols-md=2>
                             <ValidationProvider rules="required" v-slot="{ valid, errors }">
                             <b-form-input id="input-phone" :state="valid" v-model="modifyData.cellphone" type="tel" required placeholder="輸入手機"></b-form-input>
                             <b-form-invalid-feedback :state="valid">
@@ -72,20 +72,20 @@
                             </b-form-invalid-feedback>
                             </ValidationProvider>
                     </b-form-group></b-col>
-                    <b-col cols=4><b-form-group label="是否公開:" label-for="input-2" label-cols=4>
+                    <b-col cols=12 md="5"><b-form-group label="是否公開:" label-for="input-2" label-cols=4>
                         <b-form-select v-model="modifyData.permit.phone" :options="permitOption"></b-form-select>
                     </b-form-group></b-col>
                 </b-row>
-                <b-form-group label="技能專長:" label-for="input-expertise" label-cols=1>
-                    <b-form-tags id="input-expertise" placeholder="輸入技能專長(enter即可新增)" remove-on-delete size="lg" tag-variant="success" v-model="modifyData.expertiseArray"></b-form-tags>
+                <b-form-group label="技能專長:" label-for="input-expertise" label-cols=12 label-cols-md=2>
+                    <b-form-tags id="input-expertise" placeholder="輸入技能專長(enter即可新增)" remove-on-delete size="lg" tag-variant="success" v-model="modifyData.expertiseArray" tagClass="tag-block-md"></b-form-tags>
                 </b-form-group>
-                <b-form-group label="作品:" label-for="input-work" label-cols=1>
-                    <b-form-tags id="input-work" placeholder="輸入作品(enter即可新增)" remove-on-delete size="lg" tag-variant="info" v-model="modifyData.worksArray"></b-form-tags>
+                <b-form-group label="作品:" label-for="input-work" label-cols=12 label-cols-md=2>
+                    <b-form-tags id="input-work" placeholder="輸入作品(enter即可新增)" remove-on-delete size="lg" tag-variant="info" v-model="modifyData.worksArray" tagClass="tag-block-md"></b-form-tags>
                 </b-form-group>
-                <b-form-group label="證照:" label-for="input-license" label-cols=1>
-                    <b-form-tags id="input-license" placeholder="輸入證照(enter即可新增)" remove-on-delete size="lg" tag-variant="danger" v-model="modifyData.licenseArray"></b-form-tags>
+                <b-form-group label="證照:" label-for="input-license" label-cols=12 label-cols-md=2>
+                    <b-form-tags id="input-license" placeholder="輸入證照(enter即可新增)" remove-on-delete size="lg" tag-variant="danger" v-model="modifyData.licenseArray" tagClass="tag-block-md"></b-form-tags>
                 </b-form-group>
-                <b-form-group label="自我介紹:" label-for="input-biography" label-cols=1>
+                <b-form-group label="自我介紹:" label-for="input-biography" label-cols=12 label-cols-md=2>
                     <ValidationProvider rules="required" v-slot="{ valid, errors }">
                     <b-form-textarea id="input-biography" :state="valid" placeholder="介紹一下你自己" rows="8" v-model="modifyData.biography"></b-form-textarea>
                     <b-form-invalid-feedback :state="valid">
@@ -94,9 +94,9 @@
                     </ValidationProvider>
                 </b-form-group>
                 
-                <b-button v-if="valid" variant="success" @click="modify">送出</b-button>
-                <b-button v-else >送出</b-button>
-                <b-button @click="modifyCancel">取消</b-button>
+                <b-button v-if="valid" variant="success" @click="modify" class="tag-block-md mb-1">送出</b-button>
+                <b-button v-else class="tag-block-md pb-1">送出</b-button>
+                <b-button @click="modifyCancel" class="tag-block-md  pb-1">取消</b-button>
             </b-form>
         </ValidationObserver>
     </div>
@@ -191,5 +191,10 @@
 </script>
 
 <style>
-
+@media screen and (max-width: 768px) {
+    .tag-block-md{
+      display: block;
+      width: 100%;
+    }
+  }
 </style>
